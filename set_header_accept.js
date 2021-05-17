ac191eceae2aa67e7734606f8742ea563da4adc4
yaling888 <73897884+yaling888@users.noreply.github.com>
2021-05-17 22:15:22 +0800

    Update set_header_accept.js

--- set_header_accept.js
+++ set_header_accept.js
@@ -1,11 +1,9 @@
-console.log('exec set_header_accept.js')
+console.log('exec set_header_accept.js');
 
 let headers = $request.headers;
 
+console.log('set "Accept: application/vnd.github.v3.raw" in request header');
+
 headers['Accept'] = 'application/vnd.github.v3.raw';
 
-console.dir(headers)
-
 $done({headers});
-
-console.log('set "Accept: application/vnd.github.v3.raw" in request header')
