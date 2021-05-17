0f2b77c3795534a2cf8d0bf12fb0a3e7580cb9a2
yaling888 <73897884+yaling888@users.noreply.github.com>
2021-05-17 23:49:22 +0800

    Update set_header_accept.js

--- set_header_accept.js
+++ set_header_accept.js
@@ -1,12 +1,42 @@
 console.log('exec set_header_accept.js');
 
-let headers = $request.headers;
+function setHeaders() {
+    
+    let url = $request.url;
+    let token = getUrlParam(url, '_t');
+    console.log('request url: ' + url);
+    let headers = $request.headers;
+    url = removeURLParameter(url, '_t');
+  
+    console.log('set "Accept: application/vnd.github.v3.raw" in request header');
 
-console.log('request url: ' + $request.url);
-console.log('set "Accept: application/vnd.github.v3.raw" in request header');
+    headers['Authorization'] = ' token ' + token;
+    headers['Accept'] = 'application/vnd.github.v3.raw';
 
-headers['Accept'] = 'application/vnd.github.v3.raw';
+    $done({url, headers});
+    console.log('request url: ' + $request.url);
+}
 
-console.dir(headers);
 
-$done({headers});
+function getUrlParam(url, name) {
+    let reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
+    let r = url.match(reg);
+    if (r != null) return r[2]; return '';
+}
+
+function removeURLParameter(url, parameter) {
+    let urlparts = url.split('?');
+    if(urlparts.length >= 2) {
+        let prefix = parameter + '=';
+        let pars = urlparts[1].split(/[&;]/g);
+
+        for(let i = pars.length; i-- > 0;) {
+          if(pars[i].lastIndexOf(prefix, 0) !== -1) {
+            pars.splice(i, 1);
+          }
+        }
+
+        return urlparts[0] + (pars.length > 0 ? '?' + pars.join('&') : '');
+    }
+    return url;
+}
