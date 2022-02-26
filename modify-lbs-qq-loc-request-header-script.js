edf2e7d47505c9fb17530b0407a9f3250284dd5c
yaling888 <73897884+yaling888@users.noreply.github.com>
2022-02-26 16:18:39 +0800

    Update modify-lbs-qq-loc-request-header-script.js

--- modify-lbs-qq-loc-request-header-script.js
+++ modify-lbs-qq-loc-request-header-script.js
@@ -1,14 +1,4 @@
-var modifiedHeaders = $request.headers;
 
-#delete modifiedHeaders['Cookie'];
-#delete modifiedHeaders['Halley-Sequence'];
-#delete modifiedHeaders['Halley-IMEI'];
-
-#modifiedHeaders['Accept'] = 'application/json;charset=UTF-8';
-
-console.log(JSON.stringify(modifiedHeaders));
+console.log($request.headers);
 
 $done();
-
-#var modifiedPath = '/api2/abc?k=v';
-#$done({path: modifiedPath, headers : modifiedHeaders});
