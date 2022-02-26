012d5b3edb196d7085e43c8edb343cd599f5244e
yaling888 <73897884+yaling888@users.noreply.github.com>
2022-02-26 16:43:26 +0800

    Update modify-lbs-qq-loc-request-header-script.js

--- modify-lbs-qq-loc-request-header-script.js
+++ modify-lbs-qq-loc-request-header-script.js
@@ -1,5 +1,8 @@
-var headers = $request.headers;
+let headers = $request.headers;
 headers['Accept'] = 'application/json;charset=UTF-8';
+delete headers['Connection'];
+delete headers['Accept-Encoding'];
+
 console.log(JSON.stringify(headers));
 
 $done({headers: headers});
